bec169d3bcdb63a00a3cc618771d05719888fa5c
EmilianoR <emiroldanp@hotmail.com>
2021-03-12 21:59:26 -0600

    Peticiones ajax

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,7 @@
 <div class="row">
     <div class="col-12">
             @csrf
-            <input type="text" name="description" id="">
+            <input type="text" name="description" id="description">
             <input type="button" value="Crear" onclick="createTask();">
     </div>
 </div>
@@ -47,7 +47,24 @@
 @push('layout_end_body')
 <script>
     function createTask() {
-        console.log('Crea la tarea');
+        let theDescription = $('#description').val();
+        $.ajax({
+            url: '{{ route('tasks.store') }}',
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            data: {
+                description: theDescription
+            }
+        })
+        .done(function(response) {
+            console.log('Exitoso', response);
+        })
+        .fail(function(jqXHR, response) {
+            console.log('Fallido', response);
+        });
     }
 </script>
 
