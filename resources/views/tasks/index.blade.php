365ad2087e51ec56ed0f6d574cb8a3cb99ebdaf3
EmilianoR <emiroldanp@hotmail.com>
2021-03-12 21:35:05 -0600

    Juntar vistas en una

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,9 +8,39 @@
 </div>
 <div class="row">
     <div class="col-12">
-        <a href="{{ route('tasks.create') }}" class="btn btn-default">
-            Crea una tarea
-        </a>
+        <form method="POST" action="{{ route('tasks.store') }}">
+            @csrf
+            <input type="text" name="description" id="">
+            <input type="submit" value="Crear">
+        </form>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Descripcion</th>
+                    <th>¿Pendiente?</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>
+                        {{ $task->id }}
+                    </td>
+                    <td>
+                        {{ $task->description }}
+                    </td>
+                    <td>
+                        {{ $task->is_done ? 'No' : 'Si' }}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
